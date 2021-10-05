12facc35bcb9ffc9370bacdcdd788fec4b871b22
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-05 11:30:14 +0530

    Added: Fibonacci Series Program

---
+++ src/main/java/com/bdlz/logicalpblms/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package com.bdlz.logicalpblms;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        int a = 0, b = 1, c;
+        System.out.println("Enter Number");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.print(a + " " + b);
+        for (int i = 1; i < n; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
